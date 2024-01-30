d1093001386e2359e566fb06cb10def106a79823
Arsh_174 <74has@mail.ru>
2024-01-30 23:45:00 +0500

    task2

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int x = 100000;//сумма пополнения
+        if (x > 1000) {
+            int y = (x/100);
+            System.out.println(y);
+        }else {
+         int y = 0;
+        System.out.println(y);
+         }
+    }
+
+}
