1c1c254b288379e0ccf132ec80d24d3731475969
Arsh_174 <74has@mail.ru>
2024-02-01 19:16:42 +0500

    task2_2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
-    public static void main(String[] args)
-    {
-        int x = 100000;//сумма пополнения
-        if (x > 1000) {
-            int y = (x/100);
-            System.out.println(y);
-        }else {
-         int y = 0;
-        System.out.println(y);
-         }
+    public static void main(String[] args) {
+        int balans = 200;//изначальный баланс
+        int summ = 1250;//сумма пополнения
+        if (summ > 1000){
+            int bonus = (summ / 100);
+            System.out.println("Бонус равен: " + bonus);
+            System.out.println("Баланс равен: " + (summ+bonus+balans));
+        } else {
+            int bonus = 0;
+            System.out.println("Бонус равен: " + bonus);
+            System.out.println("Баланс равен: " + (summ+bonus+balans));
+        }
+
+
     }
 
 }
